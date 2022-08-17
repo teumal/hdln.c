adebd2e4117e16d26bd0e5074568234fabcaead0
Tmal <50792697+teumal@users.noreply.github.com>
2022-08-18 04:56:52 +0900

    Add files via upload

---
+++ hdln.h
@@ -0,0 +1,24 @@
+# include<stdint.h> // int64_t
+# include<string.h> // strlen, size_t
+
+                  
+
+#ifdef __cplusplus
+   # define ALIGN_AS(x) alignas(x)
+
+#else
+  # define ALIGN_AS(x) _Alignas(x)
+  
+#endif
+
+
+
+#if defined(__linux__) || defined(__unix__)
+   # include<unistd.h>  // write
+   # define WRITE write
+ 
+#elif defined(_WIN32)
+   # include<io.h>  // _write
+   # define WRITE _write
+
+#endif
