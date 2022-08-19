45cd4c1b254462775701a0c7ab64c3b466462e32
Tmal <50792697+teumal@users.noreply.github.com>
2022-08-19 21:27:35 +0900

    Update hdln.h

--- hdln.h
+++ hdln.h
@@ -1,30 +1,39 @@
 
 #ifndef HDLN // {
 
-# include<stdint.h> // int64_t
-# include<string.h> // strlen, size_t
-# define HDLN
+  # include<stdio.h>  // vsnprintf
+  # include<stdarg.h> // va_list, va_start, va_end
+  # include<stdint.h> // int64_t
+  # include<string.h> // strlen, size_t
+  # define HDLN
                   
 
-#ifdef __cplusplus
-   # define ALIGN_AS(x) alignas(x)
+  #ifdef __cplusplus
+     # define ALIGN_AS(x) alignas(x)
+     # define RESTRICT 
 
-#else
-  # define ALIGN_AS(x) _Alignas(x)
+  #else
+    # define ALIGN_AS(x) _Alignas(x)
+    # define RESTRICT RESTRICT
   
-#endif
+  #endif
 
 
 
-#if defined(__linux__) || defined(__unix__)
-   # include<unistd.h>  // write
-   # define WRITE write
+  #if defined(__linux__) || defined(__unix__)
+     # include<unistd.h>  // write
+     # define WRITE write
  
-#elif defined(_WIN32)
-   # include<io.h>  // _write
-   # define WRITE _write
+  #elif defined(_WIN32)
+     # include<io.h>  // _write
+     # define WRITE _write
 
-#endif
+  #endif
+  
+  
+  // function prototype..
+  size_t puthdln(const char* const str);             
+  size_t printhdln(const char* RESTRICT fmt, ...);  
 
 // }
 #endif
